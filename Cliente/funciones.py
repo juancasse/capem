53094bc39d03a354fd868ed97d24747212680422
Juan Casse <juan.i.cassemail.com>
2023-05-24 16:37:28 -0300

    ahora si

--- Cliente/funciones.py
+++ Cliente/funciones.py
@@ -107,7 +107,7 @@ class equiposerie():
     Clase que maneja conexion serial.
     Maneja la conexion con equipos serie, y la lectura de datos
     """
-    def __init__(self, port=None, baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=3):
+    def __init__(self, port=None, baudrate=38400, bytesize=8, parity='N', stopbits=1, timeout=3):
         self.status="desconectado"
         self.port=port
         self.baudrate=baudrate
@@ -115,7 +115,7 @@ class equiposerie():
         self.parity=parity
         self.stopbits=stopbits
         self.timeout=timeout
-        self.estado={'conexion':'desconectado','puerto':'cerrado', 'init':'sin inicializar', 'lectura':'ok', 'escritura':'ok'}
+        self.estado={'conexion':'desconectado', 'init':'sin inicializar', 'lectura':'ok', 'escritura':'ok'}
     
     def conectar(self, **kwargs):
         
@@ -147,27 +147,32 @@ class equiposerie():
             if self.con.isOpen()==False:
                 self.con.open()
             if self.con.isOpen()==True:
-                self.estado['puerto']="abierto"
+                #self.estado['puerto']="abierto"
+                pass
             else:
-                self.estado['puerto']="cerrado"
+                #self.estado['puerto']="cerrado"
+                pass
         except Exception as ex:
-            self.estado['puerto']=f'Error: {str(ex)}'
+            #self.estado['puerto']=f'Error: {str(ex)}'
+            pass
     
     def cerrar(self):
         try:
             if self.con.isOpen()==True:
                 self.con.close()
-                self.estado['puerto']="cerrado"
+                #self.estado['puerto']="cerrado"
         except Exception as ex:
-            self.estado['puerto']=f'Error: {str(ex)}'
+            #self.estado['puerto']=f'Error: {str(ex)}'
+            pass
     
     def escribir(self, datos:str):
         try:
             #Antes de escribir vacío el buffer de salida y de entrada
             self.con.flushOutput()
             self.con.flushInput()
+            dts=f'{datos}\n\r'
             if self.con.isOpen():
-                self.con.write(datos.encode("ascii"))
+                self.con.write(dts.encode())
                 self.estado['escritura']='ok'
             else:
                 self.estado['escritura']='Error: falta abrir puerto'
@@ -185,33 +190,30 @@ class equiposerie():
 
             RLS: Read lines, devuelve varias líneas en forma de lista
                 Si se especifica num, se leeran como máximo ese numero de líneas.
-                
-
+            
             R: Devuelve la cantidad de bytes especificados en num
                 Si se especifica num, se leeran ese numero de bytes. Si no, se lee uno.
                 Si no se establece timeout, se bloquea hasta que lee todos los bits especificados
         """
-        
-        
         res= None
         match tipo:
             case "RL":
                 try:
-                    res=self.con.readline(num)
-                except:
-                    pass
+                    res=self.con.readline(num).decode()
+                except Exception as e:
+                    res=str(e)
 
             case "RLS":
                 try:
-                    res=self.con.readlines(num)
-                except:
-                    pass
+                    res=self.con.readlines(num).decode()
+                except Exception as e:
+                    res=str(e)
             case "R":
                 try:
                     if num==-1: num=1
-                    res=self.con.read(num)
-                except:
-                    pass
+                    res=self.con.read(num).decode()
+                except Exception as e:
+                    res=str(e)
         return res   
 
 # %%
@@ -229,17 +231,17 @@ class LCR(equiposerie):
         Función para cambiar la frecuencia del LCR.
         No tiene tiempo de estabilizacion.
         """
-        
-        if self.con.port!=None:
-            if self.status=="desconectado":
-                self.conectar()
-        freal=freq/1000.0
-        cmd="MAIN:FREQ {:.6f}<^END^M".format(freal)
-        if self.con.isOpen()==False:
-            self.abrir()
+        try: 
+            from time import sleep
+            freal=freq/1000.0
+            cmd="MAIN:FREQ {:.5f}".format(freal)
+            print(cmd)
             self.escribir(cmd)
-            self.cerrar()
-        else: self.escribir(cmd)
+            sleep(0.5)
+            ret=self.leer('RL')
+            return ret
+        except Exception as e:
+            return str(e)
     
     def LCR_modo(self, modo:str="A"):
         """
@@ -260,62 +262,132 @@ class LCR(equiposerie):
             if modo in ["M","A"]:
                 if modo=="M": cmd="MANU"
                 else: cmd="AUTO"
-                self.escribir(f"MAIN:TRIG:{cmd}<^END^M>")
+                self.escribir(f"MAIN:TRIG:{cmd}")
 
             elif modo in ["RQ","CD","CR","LQ","LR","ZQ"]:
                 cmd=modo
-
-                self.escribir(f"MAIN:MODE:{cmd}<^END^M>")
+                self.escribir(f"MAIN:MODE:{cmd}")
         else: self.mensaje='Error: El puerto está cerrado'
 
-    def LCR_inicializar(self, **kwargs):
+    def LCR_inicializar(self,m='ON',**kwargs):
         """Ejecuta las funciones necesarias para inicializar:
             -Conectar
             -Enviar comando de iniciar comunicacion (COMU?)
             -Ver que responde correctamente(COMU:ON)
             -Enviar comando de finalizar inicializacion (COMU:OVER)
             -Ver que responde correctamente (COMU:OVER) 
-            -Setear modo automático
-        """
+            -Setear modo automático"""
+        
         if 'port' in kwargs: self.port=kwargs['port']
         if 'baudrate'in kwargs: self.baudrate=kwargs['baudrate']
         if 'bytesize'in kwargs: self.bytesize=kwargs['bytesize']
         if 'parity'in kwargs: self.parity=kwargs['parity']
         if 'stopbits'in kwargs: self.stopbits=kwargs['stopbits']
         if 'timeout' in kwargs: self.timeout=kwargs['timeout']
+        #self.estado={'conexion':'desconectado','puerto':'cerrado', 'init':'sin inicializar', 'lectura':'ok', 'escritura':'ok'}
+
         from time import sleep
         try:
-            sts=0
-            sts=self.conectar()
-            sleep(1)
-            if sts==1:
-                self.escribir('COMU?')
-                sleep(1)
-                resp=self.leer('RL')
-                if 'COMU:ON' in resp:
-                    self.escribir('COMU:OVER')
-                    sleep(1)
+            if m=='OFF':
+                self.escribir('COMU:OFF.')
+                self.inicializado=0
+                self.desconectar()
+                self.estado['conexion']='desconectado'
+                self.estado['init']='sin inicializar'
+            else:
+                sts=self.conectar()
+                sleep(0.3)
+                if sts==1:
+                    self.escribir('COMU?')
+                    sleep(0.5)
                     resp=self.leer('RL')
-                    if 'COMU:OVER' in resp:
-                        self.estado['init']='inicializado'
+                    if 'COMU:ON' in resp:
+                        self.escribir('COMU:OVER')
+                        sleep(0.5)
+                        resp=self.leer('RL')
+                        if 'COMU:OVER' in resp:
+                            self.estado['init']='inicializado'
+                            self.inicializado=1
+                        else: self.estado['init']=f'respuesta incorrecta: {resp}'
                     else: self.estado['init']=f'respuesta incorrecta: {resp}'
-
-                else: self.estado['init']=f'respuesta incorrecta: {resp}'
-            self.inicializado=1
-            
         except Exception as e:
             self.inicializado=4
             self.status=str(e)
             self.estado['init']=f'error: {str(e)}'
+        return self.estado['init']
 
+    def LCR_Configurar(self):
+        from time import sleep
+        #Frecuencia
+        res=self.LCR_cambiarfreq(12)
+        if not 'MAIN:FREQ' in res:
+            return "Error en seteo de frecuencia"
+        
+        #Velocidad
+        self.escribir('MAIN:SPEE:SLOW')
+        sleep(0.3)
+        lect=self.leer()
+        if not ('MAIN:SPEE:SLOW' in lect):
+            return "Error en cambiar velocidad"
+        
+        #Tensión
+        self.escribir('MAIN:VOLT 1.250')
+        sleep(0.3)
+        lect=self.leer()
+        if not ('MAIN:VOLT 1.250' in lect):
+            return "Error en ajustar tensión"
 
-    def medir(self):
+        #trigger
+
+        #display
+        self.escribir('MAIN:DISP:VALU')
+        #circuit
+
+        #modo (man/auto)
+
+        #modo LR
+
+        #CV
+
+        #BIAS
+        return 'configuracion exitosa'
+    def LCR_medir(self):
         """Mide los parámetros precargados del LCR"""
-        #resp=self.leer()   #Completar cuando tenga en claro como se mide
-        resp=list(1,2,3)
-        return resp
+        from time import sleep
+        self.escribir('MAIN:STAR')
+        sleep(1)
+        r1=self.leer('RL')
+        r2=self.leer('RL')
+        if 'MAIN:PRIM' in r1 and 'MAIN:SECO' in r2:
+            prim=r1[11:17]
+            uprim=r2[16:18]
+            if r2[18]=='k':
+                sec=str(round(float(r2[11:16])*100,3))
+            else:
+                sec=r2[11:16]
+            usec='Ohm'
+        else :
+            prim,uprim,sec,usec='error','error','error','error'
+        return prim,uprim,sec,usec
 
-            
+# %%
+lcr=LCR()
+lcr.LCR_inicializar(port='COM6')
 
+# %%
+lcr.LCR_cambiarfreq(50)
+
+# %%
+test=lcr.escribir('MAIN:DISP:VALU')
+test
+
+# %%
+print(test)
+
+# %%
+lcr.leer('RL')
+
+# %%
+lcr.LCR_inicializar('OFF')
 
 
